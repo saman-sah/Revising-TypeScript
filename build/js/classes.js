a06d168ba3e85bdf5a6463532bed2a71fe44135d
SamanSah <sahraei.saman91@gmail.com>
2024-04-01 14:32:37 +0330

    complete classes

--- build/js/classes.js
+++ build/js/classes.js
@@ -28,3 +28,48 @@ class WebDev extends Coder {
 }
 const Sadra = new WebDev('Sadra', 'Pop', 20, 'Win');
 console.log('Sadra', Sadra);
+class Pianist {
+    constructor(name, instrument) {
+        this.name = name;
+        this.instrument = instrument;
+    }
+    play(action) {
+        return `${this.name} ${action} the ${this.instrument}`;
+    }
+}
+class Peeps {
+    static getCount() {
+        return Peeps.count;
+    }
+    constructor(name) {
+        this.name = name;
+        this.name = name;
+        this.id = ++Peeps.count;
+    }
+}
+Peeps.count = 0;
+const John = new Peeps('John');
+const Amy = new Peeps('Amy');
+const Steave = new Peeps('Steave');
+class Bands {
+    constructor() {
+        this.dataState = [];
+    }
+    get data() {
+        return this.dataState;
+    }
+    set data(value) {
+        if (Array.isArray(value) && value.every(val => typeof val === 'string')) {
+            this.dataState = value;
+            return;
+        }
+        else {
+            throw new Error('Params is not an array of strings');
+        }
+    }
+}
+const NewBand = new Bands();
+NewBand.data = ['saman', 'sadra'];
+console.log(NewBand.data);
+NewBand.data = [...NewBand.data, 'Sara', 'Ashly'];
+console.log(NewBand.data);
--- src/classes.ts
+++ src/classes.ts
@@ -37,3 +37,72 @@ class WebDev extends Coder {
 
 const Sadra = new WebDev('Sadra', 'Pop', 20, 'Win')
 console.log('Sadra', Sadra);
+
+
+interface Musician {
+  name: string,
+  instrument: string,
+  play(action: string): string
+}
+
+class Pianist implements Musician {
+  name: string
+  instrument: string
+
+  constructor(name: string, instrument: string) {
+    this.name = name
+    this.instrument = instrument
+  }
+
+  play(action: string) {
+    return `${this.name} ${action} the ${this.instrument}`
+  }
+}
+
+
+class Peeps {
+  static count: number = 0
+
+  static getCount(): number {
+    return Peeps.count
+  }
+
+  public id: number
+
+  constructor(public name: string) {
+    this.name = name;
+    this.id = ++Peeps.count
+  }
+}
+const John = new Peeps('John')
+const Amy = new Peeps('Amy')
+const Steave = new Peeps('Steave')
+
+
+class Bands {
+  private dataState: string[]
+
+  constructor() {
+    this.dataState = []
+  }
+
+  public get data(): string[] {
+    return this.dataState
+  }
+
+  public set data(value: string[]) {
+    if (Array.isArray(value) && value.every(val => typeof val === 'string')) {
+      this.dataState = value
+      return
+    } else {
+      throw new Error('Params is not an array of strings')
+    }
+  }
+}
+
+const NewBand = new Bands()
+
+NewBand.data = ['saman', 'sadra']
+console.log(NewBand.data);
+NewBand.data = [...NewBand.data, 'Sara', 'Ashly']
+console.log(NewBand.data);
